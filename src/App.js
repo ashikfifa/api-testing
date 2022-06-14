70b2a5a927e73c6e9224a8a2998257a1c8a1b796
ashikfifa <ashikahmedfifa@gmail.com>
2022-06-14 19:41:36 +0600

    app.jsx modify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 import React,{useEffect,useState} from 'react'
 function App() {
-  const [users,setUser]=useState([])
+  const [id,setId]=useState([]);
+  const [name,setName]=useState([]);
+  const [email,setEmail]=useState([]);
+  const [address,setAddress]=useState([]);
+  const [mobile,setMobile]=useState([]);
+  const [website,setWebsite]=useState([]);
+
+
+
   useEffect(()=>{
     fetch("http://localhost:3333/lists").then((result)=>{
       result.json().then((resp)=>{
@@ -14,6 +22,23 @@ function App() {
   return (
     <div className="App">
       <h1>Get API Call </h1>
+
+
+      <input type="text" placeholder='Id' name="id"/> <br/>
+      <input type="text" placeholder='Name' name="name"/> <br/>
+      <input type="text" placeholder='Email' name="email"/> <br/>
+      <input type="text" placeholder='Address' name="address"/> <br/>
+      <input type="text" placeholder='Mobile' name="mobile"/> <br/>
+      <input type="text" placeholder='Website' name="website"/> <br/>
+
+
+      <button>SUBMIT</button>
+
+
+
+<br/><br/>
+
+
       <table border="">
        <tbody>
        <tr>
