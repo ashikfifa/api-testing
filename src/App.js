d60a98fecd4b70f73e411d01a0059045c2940cd2
ashikfifa <ashikahmedfifa@gmail.com>
2022-06-15 14:39:27 +0600

    post method

--- src/App.js
+++ src/App.js
@@ -7,32 +7,51 @@ function App() {
   const [address,setAddress]=useState([]);
   const [mobile,setMobile]=useState([]);
   const [website,setWebsite]=useState([]);
+  const [users,setUser]=useState([]);
 
 
+  function saveData()
+{
+  let data={id,name,email,address,mobile,website}
+
+  fetch("http://localhost:3333/lists", {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body:JSON.stringify(data)
+  }).then((resp)=>{
+    // console.warn("resp",resp);;
+    resp.json().then((result)=>{
+      console.warn("result",result)
+    })
+  })
+}
 
   useEffect(()=>{
     fetch("http://localhost:3333/lists").then((result)=>{
-      result.json().then((resp)=>{
-        // console.warn(resp)
-        setUser(resp)
-      })
-    })
-  },[])
-  console.warn(users)
+       result.json().then((resp)=>{
+         // console.warn(resp)
+         setUser(resp)
+       })
+   })
+   },[])
+   console.warn(users)
   return (
     <div className="App">
       <h1>Get API Call </h1>
 
 
-      <input type="text" placeholder='Id' name="id"/> <br/>
-      <input type="text" placeholder='Name' name="name"/> <br/>
-      <input type="text" placeholder='Email' name="email"/> <br/>
-      <input type="text" placeholder='Address' name="address"/> <br/>
-      <input type="text" placeholder='Mobile' name="mobile"/> <br/>
-      <input type="text" placeholder='Website' name="website"/> <br/>
+      <input type="text" placeholder='Id'value={id} onChange={(e)=>setId(e.target.value)}  name="id"/> <br/>
+      <input type="text" placeholder='Name'value={name} onChange={(e)=> setName(e.target.value)} name="name"/> <br/>
+      <input type="text" placeholder='Email'value={email} onChange={(e)=>setEmail(e.target.value)} name="email"/> <br/>
+      <input type="text" placeholder='Address'value={address} onChange={(e)=>setAddress(e.target.value)} name="address"/> <br/>
+      <input type="text" placeholder='Mobile'value={mobile} onChange={(e)=>setMobile(e.target.value)} name="mobile"/> <br/>
+      <input type="text" placeholder='Website'value={website} onChange={(e)=>setWebsite(e.target.value)} name="website"/> <br/>
 
 
-      <button>SUBMIT</button>
+      <button type='button' onClick={saveData}>SUBMIT</button>
 
 
 
